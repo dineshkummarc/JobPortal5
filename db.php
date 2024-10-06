879debe9bb403298c9fe4945ccf4ccb7778af074
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-06 09:18:49 +0530

    updated

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "job_portal";
+$dbname = "jobportal5";
 
 //Create New Database Connection
 $conn = new mysqli($servername, $username, $password, $dbname);
